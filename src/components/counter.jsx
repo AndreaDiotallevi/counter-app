96124f8cd70a5f02cf24a11499a4c38116f6d0a4
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-02-08 16:19:22 +0000

    Single source of true

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
   styles = {
     fontSize: 30
   }
@@ -14,7 +10,7 @@ class Counter extends Component {
       <div>
         <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({id:1})}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -24,25 +20,14 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = (product) =>  {
-    console.log(product);
-    this.setState({value: this.state.value + 1})
-  }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>
-
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-  }
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.value === 0) ? "warning" : "primary";
+    classes += (this.props.counter.value === 0) ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
   }
 }
