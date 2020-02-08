97ee6e342329b04d11c039acf33612f3ec8c8e7b
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-02-08 13:43:49 +0000

    composing compoennts

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    count: 0
   };
 
   styles = {
@@ -20,10 +19,6 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <div>
-          {this.state.tags.length === 0 && 'Please create a new tag!' }
-          {this.renderTags()}
-          </div>
       </div>
     );
   }
